c388ec4bed41ae0e0ca90b1ae38f9ab0ec0ac5b6
jianli-97 <lijian2@kylinos.cn>
2023-06-20 10:45:11 +0800

    添加infra-provider功能,主要实现terraform的init,apply和destroy

--- pkg/infra/openstack.go
+++ pkg/infra/openstack.go
@@ -1,10 +1,53 @@
+/*
+Copyright 2023 KylinSoft  Co., Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package infra
 
+import (
+	"gitee.com/openeuler/nestos-kubernetes-deployer/pkg/infra/terraform"
+	"github.com/hashicorp/terraform-exec/tfexec"
+)
 
 type OpenstackDeployer struct {
+	tfFilePath      string
+	terraformBinary string
 }
 
+func (t OpenstackDeployer) Create(extraOpts ...tfexec.ApplyOption) error {
+	if err := terraform.Init(t.tfFilePath, t.terraformBinary); err != nil {
+		return err
+	}
 
-func (t OpenstackDeployer) Create(spec InfraSpec, config InitConfig) error{
+	applyErr := terraform.Apply(t.tfFilePath, t.terraformBinary, extraOpts...)
+	if applyErr != nil {
+		return applyErr
+	}
+
+	return nil
+}
+
+func (t OpenstackDeployer) Destroy(extraOpts ...tfexec.DestroyOption) error {
+	if err := terraform.Init(t.tfFilePath, t.terraformBinary); err != nil {
+		return err
+	}
+
+	destroyErr := terraform.Destroy(t.tfFilePath, t.terraformBinary, extraOpts)
+	if destroyErr != nil {
+		return destroyErr
+	}
+
 	return nil
 }
