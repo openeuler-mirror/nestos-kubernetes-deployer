7ba80f4f709d1211d7efdb9419508f9317e95fa8
jianli-97 <lijian2@kylinos.cn>
2023-11-15 13:52:13 +0800

    GlobalAsset增加Load方法

--- pkg/configmanager/asset/global/globalasset.go
+++ pkg/configmanager/asset/global/globalasset.go
@@ -1,16 +1,43 @@
+/*
+Copyright 2023 KylinSoft  Co., Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package global
 
-type GlobalAsset struct {
-}
-
 var (
-	GlobalConfig GlobalAsset
+	GlobalConfig *GlobalAsset
 	IsInitial    = false
 )
 
+// ========== 包方法 ==========
+
+func GetGlobalConfig() (*GlobalAsset, error) {
+	return GlobalConfig, nil
+}
+
+// ========== 模块方法 ==========
+
+type GlobalAsset struct {
+	NKD_Version string
+}
+
 // TODO: Init inits the global asset.
 func (ga *GlobalAsset) Initial() error {
-	// TODO: 将初始化的结果传给GlobalConfig
+	ga.NKD_Version = "0.2.0"
+
+	GlobalConfig = ga
 	IsInitial = true
 	return nil
 }
