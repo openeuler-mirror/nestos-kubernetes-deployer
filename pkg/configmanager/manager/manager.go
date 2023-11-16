87b142d728d328612bd61d9e1cd13c7a5e8cfa29
jianli-97 <lijian2@kylinos.cn>
2023-11-16 09:33:21 +0800

    Improve the Initial configuration method of the management module.

--- pkg/configmanager/manager/manager.go
+++ pkg/configmanager/manager/manager.go
@@ -1,48 +1,40 @@
+/*
+Copyright 2023 KylinSoft  Co., Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package manager
 
 import (
 	"nestos-kubernetes-deployer/pkg/configmanager/asset/cluster"
 	"nestos-kubernetes-deployer/pkg/configmanager/asset/global"
-	"os"
 
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v2"
 )
 
-func Init(cmd *cobra.Command) error {
-	clusterAsset := &cluster.ClusterAsset{}
+func Initial(cmd *cobra.Command) error {
+	var globalAsset *global.GlobalAsset
+	var clusterAsset *cluster.ClusterAsset
 
-	// Parse command line arguments.
-	file, _ := cmd.Flags().GetString("file")
-
-	if file != "" {
-		// Parse configuration file.
-		yamlFile, err := os.ReadFile(file)
-		if err != nil {
-			return err
-		}
-
-		err = yaml.Unmarshal(yamlFile, clusterAsset)
-		if err != nil {
-			return err
-		}
-	}
-
-	// TODO: 设置优先级 & 参数解析
-
-	// Initialize global assets.
-	if !global.IsInitial {
-		globalAsset := &global.GlobalAsset{}
-		err := globalAsset.Initial()
-		if err != nil {
-			return nil
-		}
-	}
-
-	// Initialize cluster assets.
-	err := clusterAsset.Initial()
+	err := globalAsset.Initial(cmd)
 	if err != nil {
-		return nil
+		return err
+	}
+
+	err = clusterAsset.Initial(cmd)
+	if err != nil {
+		return err
 	}
 
 	return nil
