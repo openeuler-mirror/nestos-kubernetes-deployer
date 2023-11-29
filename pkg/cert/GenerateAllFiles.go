95712808157e4fee0792d7725a55f78d9dec17a1
duyiwei <duyiwei@kylinos.cn>
2023-11-29 14:58:46 +0800

    Modify the CA certificate generation interface

--- pkg/cert/GenerateAllFiles.go
+++ pkg/cert/GenerateAllFiles.go
@@ -17,6 +17,7 @@ limitations under the License.
 package cert
 
 import (
+	"nestos-kubernetes-deployer/pkg/configmanager"
 	"nestos-kubernetes-deployer/pkg/utils"
 
 	"github.com/sirupsen/logrus"
@@ -25,20 +26,21 @@ import (
 // GenerateAllCertificates 生成所有证书和密钥
 func GenerateAllFiles(clusterID string) ([]utils.StorageContent, error) {
 	var certs []utils.StorageContent
-	//后续引入dns、ip后再调用clusterconfig, _ := configmanager.GetClusterConfig(clusterID)
+	clusterconfig, _ := configmanager.GetClusterConfig(clusterID)
 
 	// 生成root CA 证书和密钥
-	rootCACert, err := GenerateRootCA(clusterID)
+	rootCACert, err := GenerateAllCA(clusterconfig.CertAsset.RootCaCertPath,
+		clusterconfig.CertAsset.RootCaKeyPath, "kubernetes", []string{"kubernetes"})
 	if err != nil {
 		logrus.Errorf("Error generating root CA:%v", err)
 		return nil, err
 	}
-	//保存root CA证书到宿主机,暂定为当前工作目录
+	//保存root CA证书和密钥到宿主机
 	err = SaveCertificateToFile("./", rootCACert.CertRaw)
 	if err != nil {
 		return nil, err
 	}
-	//保存root CA密钥到宿主机，暂定为当前工作目录
+
 	err = SavePrivateKeyToFile("./", rootCACert.CertRaw)
 	if err != nil {
 		return nil, err
@@ -57,10 +59,22 @@ func GenerateAllFiles(clusterID string) ([]utils.StorageContent, error) {
 	certs = append(certs, rootCACertContent, rootCAKeyContent)
 
 	// 生成 etcd CA 证书
-	etcdCACert, err := GenerateEtcdCA(clusterID)
+	etcdCACert, err := GenerateAllCA(clusterconfig.CertAsset.EtcdCaCertPath,
+		clusterconfig.CertAsset.EtcdCaKeyPath, "etcd-ca", []string{"etcd-ca"})
 	if err != nil {
 		return nil, err
 	}
+	//保存etcd-ca和密钥到宿主机
+	err = SaveCertificateToFile("./", etcdCACert.CertRaw)
+	if err != nil {
+		return nil, err
+	}
+
+	err = SavePrivateKeyToFile("./", etcdCACert.CertRaw)
+	if err != nil {
+		return nil, err
+	}
+
 	etcdCACertContent := utils.StorageContent{
 		Path:    utils.EtcdCaCrt,
 		Mode:    int(utils.CertFileMode),
@@ -74,7 +88,19 @@ func GenerateAllFiles(clusterID string) ([]utils.StorageContent, error) {
 	certs = append(certs, etcdCACertContent, etcdCAKeyContent)
 
 	// 生成 front-proxy CA 证书
-	frontProxyCACert, err := GenerateFrontProxyCA(clusterID)
+	frontProxyCACert, err := GenerateAllCA(clusterconfig.CertAsset.EtcdCaCertPath,
+		clusterconfig.CertAsset.EtcdCaKeyPath, "front-proxy-ca", []string{"front-proxy-ca"})
+	if err != nil {
+		return nil, err
+	}
+
+	//保存front-proxy-ca和密钥到宿主机
+	err = SaveCertificateToFile("./", frontProxyCACert.CertRaw)
+	if err != nil {
+		return nil, err
+	}
+
+	err = SavePrivateKeyToFile("./", frontProxyCACert.CertRaw)
 	if err != nil {
 		return nil, err
 	}
