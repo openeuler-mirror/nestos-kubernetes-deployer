cd96e614c1bad024bde860cf4f85ee21ae42d513
weihuanhuan_ky <weihuanhuan@kylinos.cn>
2024-08-27 20:35:25 +0800

    define global const,improve test cover

--- cmd/extend_test.go
+++ cmd/extend_test.go
@@ -16,18 +16,74 @@ limitations under the License.
 package cmd
 
 import (
+	"context"
 	"nestos-kubernetes-deployer/cmd/command/opts"
+	"nestos-kubernetes-deployer/pkg/configmanager/asset"
+	"nestos-kubernetes-deployer/pkg/configmanager/asset/infraasset"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestExtend(t *testing.T) {
+	globalYaml := "global_config.yaml"
 	opts.Opts.RootOptDir = "../data"
 	cmd := NewExtendCommand()
-	args := []string{"--num", "0"}
+	args := []string{"--num", "0", "--cluster-id", "k8s-007"}
 	cmd.SetArgs(args)
 	if err := cmd.Execute(); err != nil {
 		t.Logf("Failed to execute command: %v", err)
+	}
+
+	cc := &asset.ClusterAsset{
+		ClusterID:    "cluster",
+		Architecture: "amd64",
+		Platform:     "pxe",
+		InfraPlatform: infraasset.PXEAsset{
+			IP:             "",
+			HTTPServerPort: "10",
+			HTTPRootDir:    "./",
+			TFTPServerPort: "20",
+			TFTPRootDir:    "./",
+		},
+		OSImage:  asset.OSImage{Type: "nestos"},
+		UserName: "root",
+		SSHKey:   "./test.yaml",
+		Password: "123",
+		Master: []asset.NodeAsset{
+			{
+				Hostname: "k8s-master01",
+				IP:       "192.168.132.11",
+				HardwareInfo: asset.HardwareInfo{
+					CPU:  2,
+					RAM:  2048,
+					Disk: 30,
+				},
+			},
+		},
+		Worker: []asset.NodeAsset{
+			{
+				Hostname: "k8s-worker01",
+				IP:       "192.168.132.12",
+				HardwareInfo: asset.HardwareInfo{
+					CPU:  2,
+					RAM:  2048,
+					Disk: 30,
+				},
+			},
+		},
+		Runtime: "crio",
+		Kubernetes: asset.Kubernetes{
+			KubernetesVersion:    "v1.29.1",
+			KubernetesAPIVersion: "v1beta3",
+			ApiServerEndpoint:    "192.168.132.11:6443",
+			ImageRegistry:        "registry.k8s.io",
+			PauseImage:           "pause:3.9",
+			Network: asset.Network{
+				ServiceSubnet: "10.96.0.0/16",
+				PodSubnet:     "10.244.0.0/16",
+			},
+		},
 	}
 
 	t.Run("ExtendCmd Fail", func(t *testing.T) {
@@ -39,11 +95,11 @@ func TestExtend(t *testing.T) {
 			t.Logf("Failed to remove logs folder: %v", err)
 		}
 
-		if _, err := os.Stat("global_config.yaml"); os.IsNotExist(err) {
+		if _, err := os.Stat(globalYaml); os.IsNotExist(err) {
 			t.Logf("Expected global_config.yaml to be created, but it does not exist")
 		}
 
-		if err := os.Remove("global_config.yaml"); err != nil {
+		if err := os.Remove(globalYaml); err != nil {
 			t.Logf("Failed to remove global_config.yaml: %v", err)
 		}
 		// Clean up
@@ -51,12 +107,42 @@ func TestExtend(t *testing.T) {
 			t.Logf("Failed to remove logs folder: %v", err)
 		}
 
-		if _, err := os.Stat("global_config.yaml"); os.IsNotExist(err) {
+		if _, err := os.Stat(globalYaml); os.IsNotExist(err) {
 			t.Logf("Expected global_config.yaml to be created, but it does not exist")
 		}
 
-		if err := os.Remove("global_config.yaml"); err != nil {
+		if err := os.Remove(globalYaml); err != nil {
 			t.Logf("Failed to remove global_config.yaml: %v", err)
+		}
+	})
+	t.Run("extendCluster Fail", func(t *testing.T) {
+		err := extendCluster(cc, 1)
+		if err != nil {
+			t.Log(err)
+		}
+	})
+	t.Run("extendArray Fail", func(t *testing.T) {
+		err := extendArray(cc, 1)
+		if err != nil {
+			t.Log(err)
+		}
+	})
+	t.Run("checkNodesReady Fail", func(t *testing.T) {
+		err := checkNodesReady(context.Background(), cc, 1)
+		if err != nil {
+			t.Log(err)
+		}
+	})
+	//t.Run("extendCluster Fail", func(t *testing.T) {
+	//	_, err := getReadyNodesCount(context.Background(), nil)
+	//	if err != nil {
+	//		t.Log(err)
+	//	}
+	//})
+	t.Run("waitForMinimumReadyNodes Fail", func(t *testing.T) {
+		err := waitForMinimumReadyNodes(context.Background(), nil, 1, time.Second)
+		if err != nil {
+			t.Log(err)
 		}
 	})
 }
