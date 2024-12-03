d84f46c061e9234c41f9dda407b56b02fa3efec1
lauk <liukuo@kylinos.cn>
2024-12-03 18:08:36 +0800

    Add a copyright notice for the code

--- data/assets.go
+++ data/assets.go
@@ -3,6 +3,14 @@
 
 //go:generate go run assets_generate.go
 
+/*
+ * Copyright (c) KylinSoft  Co., Ltd. 2024.All rights reserved.
+ * nestos-kubernetes-deployer licensed under the Apache License, Version 2.0.
+ * See LICENSE file for more details.
+ * Author: liukuo <liukuo@kylinos.cn>
+ * Date: Thu Jul 25 16:18:53 2024 +0800
+ */
+
 package data
 
 import (
